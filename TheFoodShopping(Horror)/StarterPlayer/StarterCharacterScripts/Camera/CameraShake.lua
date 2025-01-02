dd9f76514fcff47d1a8d82f591d9918bc6cf3b4c
Martimf95 <martims766@gmail.com>
2025-01-02 22:23:08 +0100

    V1.2

--- TheFoodShopping(Horror)/StarterPlayer/StarterCharacterScripts/Camera/CameraShake.lua
+++ TheFoodShopping(Horror)/StarterPlayer/StarterCharacterScripts/Camera/CameraShake.lua
@@ -8,26 +8,26 @@
     Last Edited By: Martimf95
     Last Edit Date: 02.01.2025
     
-    Version: V1.0
+    Version: V1.1
 
-    Credits: Martimf95[V1.0]
+    Credits: Martimf95[V1.0]-[V1.1]
 --]]
 
 local runService = game:GetService("RunService")
-local character = script.Parent
+local character = script.Parent.Parent
 local humanoid = character:WaitForChild("Humanoid")
 
-function updCameraShake()
+function updateCameraShake()
 	local currentTime = tick()
 	if humanoid.MoveDirection.Magnitude > 0 then 
 		local shakeX = math.cos(currentTime * 10) * .5
 		local shakeY = math.abs(math.sin(currentTime * 10)) * .8
 		local shake = Vector3.new(shakeX, shakeY, 0)
 
-		humanoid.CameraOffset = humanoid.CameraOffset:lerp(shake, .25)
+		humanoid.CameraOffset = humanoid.CameraOffset:lerp(shake, .4)
 	else
 		humanoid.CameraOffset = humanoid.CameraOffset * .9
 	end
 end
 
-runService.RenderStepped:Connect(updCameraShake)
+runService.RenderStepped:Connect(updateCameraShake)
